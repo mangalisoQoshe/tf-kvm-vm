0c0957676134476aa19c3c500e08a9a8a2c8049f
mangalisoQoshe <michael.qoshe98@gmail.com>
2026-03-26 18:19:29 +0200

    update docs & replace path.module with path.root

--- outputs.tf
+++ outputs.tf
@@ -11,8 +11,30 @@ output "vm_ip" {
   value       = length(data.libvirt_domain_interface_addresses.ubuntu.interfaces) > 0 && length(data.libvirt_domain_interface_addresses.ubuntu.interfaces[0].addrs) > 0 ? data.libvirt_domain_interface_addresses.ubuntu.interfaces[0].addrs[0].addr : "No IP address found"
 }
 
-output "module_path" {  
-  
-  value = "${path.cwd}"
+output "instructions" {
+  value = <<-EOF
 
-} 
+    Virtual machines have been created!
+
+    To find IP addresses assigned by DHCP:
+      sudo virsh domifaddr ubuntu-vm
+      
+
+    Or check the DHCP leases:
+      sudo virsh net-dhcp-leases default
+
+    To connect via SSH (once you know the IP):
+      ssh admin@<IP-ADDRESS>
+      Password: ******
+
+    To view VM console:
+      sudo virsh console ubuntu-vm
+
+    To connect via VNC:
+      1. Find VNC port: sudo virsh domdisplay ubuntu-vm
+      2. Connect with VNC client to that address
+
+    Note: It may take 30-60 seconds after boot for cloud-init to complete
+          and the SSH server to be available.
+  EOF
+}
